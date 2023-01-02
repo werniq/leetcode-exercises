3c3da5064606a83c699a16cd746125eb2e5ced44
qniwerss <73220736+werniq@users.noreply.github.com>
2023-01-02 21:48:04 +0100

    Create valid_sudoku.go
    It basically checks if:
    - A number is not printed twice in row, column, or 3x3 cell

---
+++ leetcode/valid_sudoku.go
@@ -0,0 +1,82 @@
+import "strconv"
+
+func isValidSudoku(board [][]byte) bool {
+
+
+	for i := 0; i < 9; i++ {
+		tmp := [10]int{}
+		for j := 0; j < 9; j++ {
+			cellVal := board[i][j : j+1]
+			if string(cellVal) != "." {
+				index, _ := strconv.Atoi(string(cellVal))
+                    
+				if tmp[index] == 1 {
+					return false
+				}
+				tmp[index] = 1
+			}
+		}
+	}
+    
+	for i := 0; i < 9; i++ {
+		tmp := [10]int{}
+		for j := 0; j < 9; j++ {
+			cellVal := board[j][i]
+			if string(cellVal) != "." {
+				index, _ := strconv.Atoi(string(cellVal))
+				if index > 9 || index < 1 {
+					return false
+				}
+				if tmp[index] == 1 {
+					return false
+				}
+				tmp[index] = 1
+			}
+		}
+	}
+
+
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			tmp := [10]int{}
+			for ii := i * 3; ii < i*3+3; ii++ {
+				for jj := j * 3; jj < j*3+3; jj++ {
+					cellVal := board[ii][jj]
+					if string(cellVal) != "." {
+						index, _ := strconv.Atoi(string(cellVal))
+						if tmp[index] == 1 {
+							return false
+						}
+						tmp[index] = 1
+					}
+				}
+			}
+		}
+	}
+	return true
+}
+
+
+func isValidSudoku1(board [][]byte) bool {
+	rowbuf, colbuf, boxbuf := make([][]bool, 9), make([][]bool, 9), make([][]bool, 9)
+	for i := 0; i < 9; i++ {
+		rowbuf[i] = make([]bool, 9)
+		colbuf[i] = make([]bool, 9)
+		boxbuf[i] = make([]bool, 9)
+	}
+    
+	for r := 0; r < 9; r++ {
+		for c := 0; c < 9; c++ {
+			if board[r][c] != '.' {
+				num := board[r][c] - '0' - byte(1)
+				if rowbuf[r][num] || colbuf[c][num] || boxbuf[r/3*3+c/3][num] {
+					return false
+				}
+				rowbuf[r][num] = true
+				colbuf[c][num] = true
+				boxbuf[r/3*3+c/3][num] = true // r,c 转换到box方格中
+			}
+		}
+	}
+	return true
+}
